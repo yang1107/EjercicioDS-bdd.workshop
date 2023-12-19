00fa70aa93fc31ec9df39a1d1941a1edc06f931e
yang chen <244381442@qq.com>
2023-12-19 15:42:51 +0100

    MCV
    agregar MCV

---
+++ bdd.workshop.calculator.web/Controllers/SquareRootController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bdd.workshop.calculator.web.Controllers
+{
+    public class SquareRootController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SquareRoot(Models.SquareRoot squareRoot)
+        {
+            ViewData["a"] = squareRoot.A.TheNumber;
+            ViewData["result"] = Operator.SquareRoot(squareRoot.A.TheNumber);
+            return View();
+        }
+    }
+}
---
+++ bdd.workshop.calculator.web/Models/SquareRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bdd.workshop.calculator.web.Models
+{
+    public class SquareRoot
+    {
+        public Number A { get; set; } = new Number();
+
+    }
+}
